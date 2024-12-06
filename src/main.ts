0a5b6cd60c75ee30aa3dcde40cb482f54bd89cfe
Eduardo <eduardolagonunes@gmail.com>
2024-12-06 14:59:24 -0300

    DEPLOY

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: 'http://localhost:5173',
+    origin: 'https://processo-seletivo-teddy.netlify.app',
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
     allowedHeaders: ['Content-Type', 'Authorization'],
   });
